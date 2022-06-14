e6ee22bdc9c298af41142d73ea969aee10eca5aa
cabraldasilvac <wcabraldasilvac@gmail.com>
2022-06-14 19:58:14 -0300

    Atualizando a página tempo

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import Link from 'next/link';
+
 function Tempo(props) {
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
