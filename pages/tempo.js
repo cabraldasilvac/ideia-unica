906c0745a3ee92a9104433279302564064bb333c
cabraldasilvac <wcabraldasilvac@gmail.com>
2022-06-14 20:28:31 -0300

    adiciona delay

--- pages/tempo.js
+++ pages/tempo.js
@@ -10,13 +10,16 @@ return
 (
       <div>
         <div> {dynamicDateString}(dinâmico)</div>
-        <div>{props.staticDateString}(estático)</div>
+        <div>{props.staticDateString}(estático - com delay )</div>
       </div>
 )
 }
 
-export function getStaticProps() {
-    console.log('> Passando pelo getStaticProps().')
+export async function getStaticProps() {
+    console.log('> Passando pelo getStaticProps()');
+    console.log('> Adicionando delay de 5 segundos');
+
+    await delay (5000);
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
@@ -26,4 +29,5 @@ export function getStaticProps() {
         }
     }
 }
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 export default Tempo;
