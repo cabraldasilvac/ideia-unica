946156bdd2130f5903ec67bf61ddacce00eb777a
cabraldasilvac <wcabraldasilvac@gmail.com>
2022-06-14 19:19:59 -0300

    updates

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 
 function Home(){
-    return(<div><h1>Home</h1>
+    return(<div><h1>Página Home</h1>
     <Link href="/sobre">
         <a>Acessar página Sobre</a>
         </Link>
--- pages/sobre.js
+++ pages/sobre.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 
 function Sobre(){
-    return(<div><h1>Sobre</h1>
+    return(<div><h1>Página Sobre</h1>
     <Link href="/">
         <a>Acessar página Home</a>
         </Link>
