1164a527c7a69e3961addf777248ba9e14d38e97
cabraldasilvac <wcabraldasilvac@gmail.com>
2022-06-15 17:29:49 -0300

    updates

--- pages/produtos/[id].js
+++ pages/produtos/[id].js
@@ -9,9 +9,6 @@ export async function getServerSideProps(context){
 }
 */
 
-import { useState } from 'react';
-import Link from 'next/link';
-
 export async function getStaticPaths(){
   return{
     paths: [{
@@ -29,7 +26,7 @@ export async function getStaticPaths(){
 }
 
 export async function getStaticProps(context){
-  // await delay(5000);
+  await delay(5000);
   const id = context.params.id; 
 
   return {
@@ -44,8 +41,8 @@ function Produtos(props){
   return <div>Id do Produto: {props.id}</div>
 }
 export default Produtos;
-/*
+
 function delay(ms){
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-*/
+
