f4469f9614506b6bb9847ce6236e1f0d89ec6227
cabraldasilvac <wcabraldasilvac@gmail.com>
2022-06-16 12:02:09 -0300

    update files

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -3,8 +3,8 @@ function tempo(request, response) {
   const apiSecret = process.env.CONVERTKIT_API_SECRET; 
   const dynamicDate = new Date();
 
-  const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
-  const subscribersResponseJson = await subscribersResponse.json();
+  //const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+  //const subscribersResponseJson = await subscribersResponse.json();
   const inscritos = subscribersResponseJson.total_subscribers;
 
   response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
