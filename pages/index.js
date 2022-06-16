148fc38ea664cede23cc12f1e3a72c95d7f01906
cabraldasilvac <wcabraldasilvac@gmail.com>
2022-06-16 14:39:13 -0300

    my version 1.5

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 function Home(){
     return(<div><h1>Home</h1>
     <Link href="/sobre">
-        <a>Acessar página Sobre</a>
+        <a>Acessar a página Sobre</a>
         </Link>
         <br/><Contador />   
     </div>)
