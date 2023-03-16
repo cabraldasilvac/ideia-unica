074684a19f8903b89381e22df4b7e8bf8ef97b1f
cabraldasilvac <wcabraldasilvac@gmail.com>
2023-03-16 19:47:01 -0300

    feat: Update file

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,43 @@
-import { useState } from 'react';
-import Link from 'next/link';
+import { useState } from "react";
+import Link from "next/link";
 
-function Home(){
-    return(<div><h1>Página  Home</h1>
-     <h3> <font color="red">Se você chegou até aqui saiba que esta Página é somente para testes.</font></h3><br/>
-    <Link href="/sobre">
+function Home() {
+  return (
+    <div>
+      <h1>Página Home</h1>
+      <h3>
+        {" "}
+        <font color="red">
+          Você chegou até aqui, saiba que esta Página é somente para
+          treinamentos.
+        </font>
+      </h3>
+      <br />
+      <Link href="/sobre">
         <a>Acessar a página Sobre</a>
-        </Link><br/>    
-        <br/><Contador />   
-    </div>)
+      </Link>
+      <br />
+      <br />
+      <Contador />
+    </div>
+  );
 }
 
 function Contador() {
-    const [contador, setContador] = useState(1);
+  const [contador, setContador] = useState(1);
 
-    function adicionarContador() {
-        setContador(contador++);
-    }
-        return ( 
-          <div>
-              <div> {contador} 
-              <button onClick = { adicionarContador } > Adicionar </button>  
-              </div>         
-          </div>
-    )
+  function adicionarContador() {
+    setContador(contador++);
+  }
+  return (
+    <div>
+      <div>
+        {" "}
+        {contador}
+        <button onClick={adicionarContador}> Adicionar </button>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
